61af6b11a2b0d16afc5d7bad5b8a9daeabb72304
Sara Baqla <missatrox44@gmail.com>
2024-07-06 22:30:49 -0500

    billing page link, rendering on home page

---
+++ components/Billing/Billing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Billing() {
+  return (
+    <section id="billing">
+      {/* Main container */}
+      <div className="flex flex-col md:flex-row bg-lilac rounded-2xl py-14 px-11">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima mollitia recusandae earum est eius obcaecati a error assumenda? Veritatis officia deleniti dolorem, odio optio vel ducimus quaerat, repellat alias dignissimos modi! Minima, temporibus cum! Nisi eos tempore tempora nam obcaecati ab, recusandae excepturi accusantium sint molestiae error in enim voluptate.
+      </div>
+    </section>
+  );
+}
---
+++ components/index.ts
@@ -0,0 +1,1 @@
+import Billing from "./Billing/Billing";
