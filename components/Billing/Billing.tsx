ee87d7e928a9bdc5d6502020f16c91826702a2e7
Sara Baqla <missatrox44@gmail.com>
2024-07-07 09:59:09 -0500

    left side menu

--- components/Billing/Billing.tsx
+++ components/Billing/Billing.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { MdCropDin, MdLock, MdEmail, MdCreditCard, MdApps } from "react-icons/md";
+import { MdCropDin, MdLock, MdEmail, MdCreditCard, MdApps, MdLogout } from "react-icons/md";
 
 export default function Billing() {
   const menuItems = [
     { name: 'General', icon: MdCropDin, isSelected: false },
-    { name: 'Password', icon: MdLock, isSelected: false  },
-    { name: 'Invitations', icon: MdEmail, isSelected: false  },
+    { name: 'Password', icon: MdLock, isSelected: false },
+    { name: 'Invitations', icon: MdEmail, isSelected: false },
     { name: 'Billing', icon: MdCreditCard, isSelected: true },
-    { name: 'Apps', icon: MdApps, isSelected: false  },
+    { name: 'Apps', icon: MdApps, isSelected: false },
   ];
 
   return (
@@ -22,20 +22,29 @@ export default function Billing() {
             <img src="./images/headshot.jpg" alt="" className="rounded-xl w-24" />
             <div className="flex flex-col ml-4">
               <p className="font-bold text-2xl">Ildiko Gaspar</p>
-              <p className="text-light text-2xl text-gray-400">@igaspar</p>
+              <p className="font-light text-2xl text-gray-400">@igaspar</p>
             </div>
           </div>
 
-          <ul className="pl-10 text-gray-500 text-xl space-y-5">
+          <ul className="pl-2 text-gray-500 text-lg space-y-7 font-light mb-72">
             {menuItems.map((item, index) => (
-              <li key={index} className="flex items-center space-x-4">
-                <div className="h-10 w-10">
+              <li
+                key={index}
+                className={`flex items-center space-x-4 pl-8  ${item.isSelected ? 'border-l border-l-orangeish border-l-[6px] ' : ''}`}
+              >
+                <div className={`h-10 w-10  ${item.isSelected ? 'ml-[-5px]' : ''}`}>
                   <item.icon className="h-full w-full" />
                 </div>
                 <p>{item.name}</p>
               </li>
             ))}
           </ul>
+          <div className="flex items-center text-gray-500 space-x-4 pl-10 mb-10">
+            <div className="h-10 w-10">
+              <MdLogout className="h-full w-full" />
+            </div>
+            <p className="font-light text-lg">Log out</p>
+          </div>
         </div>
         {/* <div className="bg-white rounded-r-2xl">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo accusantium, esse explicabo quasi numquam dolor delectus fugit reiciendis inventore quaerat assumenda. Consequuntur fuga ab accusamus officia dignissimos vel laudantium ea.
