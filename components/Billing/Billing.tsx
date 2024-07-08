7cf1833f41db26fd2f31749448842428db8d031f
Sara Baqla <missatrox44@gmail.com>
2024-07-07 19:23:08 -0500

    created mobile message

--- components/Billing/Billing.tsx
+++ components/Billing/Billing.tsx
@@ -21,11 +21,15 @@ export default function Billing() {
     <section id="billing">
       {/* Main container */}
       <div className="flex min-h-screen items-center justify-center bg-lilac">
-        <div className="flex shadow-2xl rounded-2xl relative">
+        <h2 className="lg:hidden text-center font-bold text-3xl px-10">
+          This component is only optimized for Desktop. Please enlarge your
+          window.
+        </h2>
+        <div className=" shadow-2xl rounded-2xl relative hidden lg:flex">
           {/* background rectangle */}
-        <div className="absolute bg-orangeish w-[300px] h-full left-[-1.25rem] top-[-1.25rem] z-0 rounded-2xl"></div>
+
+          <div className="absolute bg-orangeish w-[300px] h-full left-[-1.25rem] top-[-1.25rem] z-0 rounded-2xl"></div>
           <div className="z-20 bg-lighterlilac rounded-l-2xl w-fit py-8 ">
-   
             {/* user info */}
             <div className=" px-10 pb-20 flex items-center justify-center">
               <img
@@ -43,10 +47,13 @@ export default function Billing() {
               {menuItems.map(({ isSelected, icon: Icon, menuName }, index) => (
                 <li
                   key={index}
-                  className={`flex items-center space-x-4 pl-8  ${isSelected ? "border-l-[6px] border-l-orangeish" : ""
-                    }`}
+                  className={`flex items-center space-x-4 pl-8  ${
+                    isSelected ? "border-l-[6px] border-l-orangeish" : ""
+                  }`}
                 >
-                  <div className={`h-10 w-10  ${isSelected ? "ml-[-5px]" : ""}`}>
+                  <div
+                    className={`h-10 w-10  ${isSelected ? "ml-[-5px]" : ""}`}
+                  >
                     <Icon className="h-full w-full" />
                   </div>
                   <p>{menuName}</p>
@@ -87,25 +94,7 @@ export default function Billing() {
                       </td>
                     </tr>
                     <tr className="hover:shadow-xl rounded-2xl">
-                    <td className="py-3 pr-20 pl-4">Oct. 21, 2024</td>
-                    <td className="py-3 pr-20 ">Pro Annual</td>
-                      <td className="py-3 pr-4 ">
-                        <button className="px-3 py-2 rounded-lg border-2 border-gray-300 focus:bg-orangeish focus:text-white">
-                          Download
-                        </button>
-                      </td>
-                    </tr>
-                    <tr className="hover:shadow-xl rounded-2xl">
-                    <td className="py-3 pr-20 pl-4">Oct. 21, 2024</td>
-                    <td className="py-3 pr-20 ">Pro Annual</td>
-                      <td className="py-3 pr-4 ">
-                        <button className="px-3 py-2 rounded-lg border-2 border-gray-300 focus:bg-orangeish focus:text-white">
-                          Download
-                        </button>
-                      </td>
-                    </tr>
-                    <tr className="hover:shadow-xl rounded-2xl">
-                    <td className="py-3 pr-20 pl-4">Oct. 21, 2024</td>
+                      <td className="py-3 pr-20 pl-4">Oct. 21, 2024</td>
                       <td className="py-3 pr-20 ">Pro Annual</td>
                       <td className="py-3 pr-4 ">
                         <button className="px-3 py-2 rounded-lg border-2 border-gray-300 focus:bg-orangeish focus:text-white">
@@ -113,29 +102,50 @@ export default function Billing() {
                         </button>
                       </td>
                     </tr>
-
+                    <tr className="hover:shadow-xl rounded-2xl">
+                      <td className="py-3 pr-20 pl-4">Oct. 21, 2024</td>
+                      <td className="py-3 pr-20 ">Pro Annual</td>
+                      <td className="py-3 pr-4 ">
+                        <button className="px-3 py-2 rounded-lg border-2 border-gray-300 focus:bg-orangeish focus:text-white">
+                          Download
+                        </button>
+                      </td>
+                    </tr>
+                    <tr className="hover:shadow-xl rounded-2xl">
+                      <td className="py-3 pr-20 pl-4">Oct. 21, 2024</td>
+                      <td className="py-3 pr-20 ">Pro Annual</td>
+                      <td className="py-3 pr-4 ">
+                        <button className="px-3 py-2 rounded-lg border-2 border-gray-300 focus:bg-orangeish focus:text-white">
+                          Download
+                        </button>
+                      </td>
+                    </tr>
                   </tbody>
                 </table>
-                <a href="#" className="text-orangeish hover:underline pl-4">Load more</a>
+                <a href="#" className="text-orangeish hover:underline pl-4">
+                  Load more
+                </a>
 
                 <div className="mt-20">
                   <p className="font-bold pl-4">Payment Method</p>
                   <p className="font-light text-sm mb-4 pl-4">
-                    Manage billing information and view receipts
+                    Lorem ipsum dolor sit adipiscing elit
                   </p>
-            
 
-                    <div className="pl-4 flex items-center">
-                      <div className="bg-white w-20 border p-2 rounded mr-4">
-                        <img className="w-full" src="/images/visa-logo.png" alt="visa logo" />
-                      </div>
-                      <p className="font-semibold mr-20">Visa ending in 2255</p>
-                    <button className="px-3 py-2 rounded-lg border-2 border-gray-300 focus:bg-orangeish focus:text-white justify-end">
-                          Remove
-                        </button>
+                  <div className="pl-4 flex items-center">
+                    <div className="bg-white w-20 border p-2 rounded mr-4">
+                      <img
+                        className="w-full"
+                        src="/images/visa-logo.png"
+                        alt="visa logo"
+                      />
                     </div>
+                    <p className="font-semibold mr-20">Visa ending in 2255</p>
+                    <button className="px-3 py-2 rounded-lg border-2 border-gray-300 focus:bg-orangeish focus:text-white justify-end">
+                      Remove
+                    </button>
                   </div>
-   
+                </div>
               </div>
               <div className="flex flex-col bg-orangeish text-white p-6 rounded-2xl h-fit">
                 <p className="text-sm font-light mb-3">Your plan</p>
