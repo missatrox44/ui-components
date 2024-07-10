01e5702694f3651c448069054b3b924ba8fd93c3
Sara Baqla <missatrox44@gmail.com>
2024-07-09 20:25:59 -0500

    add pricing card data

---
+++ components/PricingCards/pricingCardsData.ts
@@ -0,0 +1,28 @@
+export const pricingCardsData = [
+  {
+    id: 0,
+    title: "Free",
+    price: "0",
+    subText: "Free for your whole team",
+    description: "For individuals or teams looking to organize anything.",
+    buttonText: "Get started",
+    buttonColor: "pastelFuschia",
+  },
+  {
+    id: 1,
+    title: "Standard",
+    price: "6",
+    subText: "Per user per month",
+    description: "For teams that need to manage more work.",
+    buttonText: "Upgrade Now",
+    buttonColor: "pastelBlue",
+  },
+  {
+    id: 2,
+    title: "Premium",
+    price: "12",
+    description: "Per user per month",
+    buttonText: "Best for teams that need to track multiple projects.",
+    buttonColor: "pastelOrange",
+  },
+]
