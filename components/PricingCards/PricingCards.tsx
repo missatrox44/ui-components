0be1928f97e8f2073365a826ca4ef1eda2779638
Sara Baqla <missatrox44@gmail.com>
2024-07-09 21:00:15 -0500

    aligned buttons

--- components/PricingCards/PricingCards.tsx
+++ components/PricingCards/PricingCards.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { pricingCardsData } from "./pricingCardsData";
 import classNames from "classnames";
+import styles from "./styles.module.css";
 
 const buttonColorClasses: Record<string, string> = {
   pastelFuschia: 'bg-pastelFuschia',
@@ -11,31 +12,32 @@ const buttonColorClasses: Record<string, string> = {
 export default function PricingCards() {
   return (
     <section id="pricing-cards">
-      <div className="flex min-h-screen items-center justify-center px-10  bg-lilac">
+      <div className="flex min-h-screen items-center justify-center px-10 bg-lilac">
         <div className="bg-white p-4 rounded-xl">
-          <div className="grid grid-cols-1 md:grid-cols-3 p-10 bg-lilac rounded-xl gap-x-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 p-10 bg-amber-50 rounded-xl gap-x-6 gap-y-6">
             {pricingCardsData.map((card) => (
-              <div key={card.id} className="">
+              <div key={card.id} className="flex flex-col p-6 ">
                 <h2 className="font-serif font-semibold text-xl mb-5 text-gray-700">{card.title}</h2>
                 <div className="flex space-x-1">
                   <p className="font-semibold text-xl self-end">$</p>
                   <p className="font-bold text-4xl">{card.price}</p>
                 </div>
                 <p className="text-xs font-semibold leading-loose text-gray-700">{card.subText}</p>
-                <p className="text-sm font-semibold text-gray-700 pt-6 pb-11">{card.description}</p>
+                <p className="text-sm font-semibold text-gray-700 pt-6 pb-11 flex-grow">{card.description}</p>
                 <a
                   href="#"
                   className={classNames(
-                    "text-black p-4 rounded-lg w-full shadow-sm hover:bg-opacity-90 hover:shadow-lg transition hover:-translate-y-0.5 duration-150 text-sm font-semibold",
+                    "text-black p-4 rounded-lg w-full shadow-sm hover:bg-opacity-90 hover:shadow-lg transition hover:-translate-y-0.5 duration-150 text-sm font-semibold mt-auto text-center",
                     buttonColorClasses[card.buttonColor]
                   )}
-                >{card.buttonText}</a>
+                >
+                  {card.buttonText}
+                </a>
               </div>
             ))}
           </div>
         </div>
       </div>
-
     </section>
   );
 }
