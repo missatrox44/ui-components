5397df059b9e95fcdbb88696b3221dd5d4086077
Sara Baqla <missatrox44@gmail.com>
2024-09-28 16:52:55 -0500

    title text styles

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -5,7 +5,9 @@ import styles from "./styles.module.css";
 export default function Login() {
   return (
     <section id="login" className={classNames("min-h-screen flex flex-col justify-center items-center", styles.login)}>
-      <h1>PROTECT YOUR DATA WITH US</h1>
+      <div className="w-full md:w-1/4">
+      <h1 className={styles["title-text"]}>PROTECT YOUR DATA WITH US</h1>
+      </div>
       <p>230,100+ users trust us!</p>
     </section>
   );
