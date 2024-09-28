71f22f876ca0bbc4f30688256516e2dd9f2e8529
Sara Baqla <missatrox44@gmail.com>
2024-09-28 16:47:48 -0500

    background image

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import {
-  MdCropDin,
-  MdLock,
-  MdEmail,
-  MdCreditCard,
-  MdApps,
-  MdLogout,
-} from "react-icons/md";
 import classNames from "classnames";
 import styles from "./styles.module.css";
 
 export default function Login() {
- 
   return (
-    <section id="login">
-      <div>LOGIN </div>
+    <section id="login" className={classNames("min-h-screen flex flex-col justify-center items-center", styles.login)}>
+      <h1>PROTECT YOUR DATA WITH US</h1>
+      <p>230,100+ users trust us!</p>
     </section>
   );
 }
